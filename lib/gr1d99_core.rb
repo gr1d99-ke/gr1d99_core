bb01a9436e93f628558266fa1d79bd6d9c89a369
Gideon Kimutai <gideonkimutai9@gmail.com>
2019-05-04 11:58:53 +0300

    add helpers

--- lib/gr1d99_core.rb
+++ lib/gr1d99_core.rb
@@ -1,4 +1,18 @@
 require "gr1d99_core/railtie"
 require "gr1d99_core/jwt"
 
-module Gr1d99Core; end
+module Gr1d99Core
+  module Helpers
+    def self.jwt_config
+      JWTConfig.new(key: 123, verify: true, algorithm: 'HS512', exp: nil)
+    end
+
+    def self.auth_required
+      p jwt_config
+      auth_header = 'HTTP_AUTHORIZATION'
+      jwt_token = request.env[auth_header].split(' ')[-1]
+      decoded_jwt_data = JWT.decode(jwt_token, jwt_conf)
+
+    end
+  end
+end
--- test/gr1d99_core_test.rb
+++ test/gr1d99_core_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class Gr1d99Core::Test < ActiveSupport::TestCase
   test "truth" do
+    p Gr1d99Core::Helpers.auth_required
     assert_kind_of Module, Gr1d99Core
   end
 end
